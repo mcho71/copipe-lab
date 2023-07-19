d5397d5da058e54980791fc5e0de37639f382abd
mcho71 <machida.city7110@gmail.com>
2023-07-19 15:14:34 +0900

    platform event

---
+++ force-app/main/default/lwc/platformEventSample/platformEventSample.js
@@ -0,0 +1,37 @@
+import { LightningElement, track } from "lwc";
+import { subscribe } from "lightning/empApi";
+import publish from "@salesforce/apex/PlatformEventService.publish";
+
+const channel = "/event/CopipeLabEvent__e";
+const COMMAND_DELAY = 1000;
+// prettier-ignore
+const nameList = [
+  'Time', 'Past', 'Future', 'Dev', 'Fly', 'Flying', 'Soar', 'Soaring', 'Power', 'Falling', 'Fall', 'Jump', 'Cliff', 'Mountain', 'Rend', 'Red', 'Blue', 'Green', 'Yellow', 'Gold', 'Demon', 'Demonic', 'Panda', 'Cat', 'Kitty', 'Kitten', 'Zero', 'Memory', 'Trooper', 'XX', 'Bandit', 'Fear', 'Light', 'Glow', 'Tread', 'Deep', 'Deeper', 'Deepest', 'Mine', 'Your', 'Worst', 'Enemy', 'Hostile', 'Force', 'Video', 'Game', 'Donkey', 'Mule', 'Colt', 'Cult', 'Cultist', 'Magnum', 'Gun', 'Assault', 'Recon', 'Trap', 'Trapper', 'Redeem', 'Code', 'Script', 'Writer', 'Near', 'Close', 'Open', 'Cube', 'Circle', 'Geo', 'Genome', 'Germ', 'Spaz', 'Shot', 'Echo', 'Beta', 'Alpha', 'Gamma', 'Omega', 'Seal', 'Squid', 'Money', 'Cash', 'Lord', 'King', 'Duke', 'Rest', 'Fire', 'Flame', 'Morrow', 'Break', 'Breaker', 'Numb', 'Ice', 'Cold', 'Rotten', 'Sick', 'Sickly', 'Janitor', 'Camel', 'Rooster', 'Sand', 'Desert', 'Dessert', 'Hurdle', 'Racer', 'Eraser', 'Erase', 'Big', 'Small', 'Short', 'Tall', 'Sith', 'Bounty', 'Hunter', 'Cracked', 'Broken', 'Sad', 'Happy', 'Joy', 'Joyful', 'Crimson', 'Destiny', 'Deceit', 'Lies', 'Lie', 'Honest', 'Destined', 'Bloxxer', 'Hawk', 'Eagle', 'Hawker', 'Walker', 'Zombie', 'Sarge', 'Capt', 'Captain', 'Punch', 'One', 'Two', 'Uno', 'Slice', 'Slash', 'Melt', 'Melted', 'Melting', 'Fell', 'Wolf', 'Hound', 'Legacy', 'Sharp', 'Dead', 'Mew', 'Chuckle', 'Bubba', 'Bubble', 'Sandwich', 'Smasher', 'Extreme', 'Multi', 'Universe', 'Ultimate', 'Death', 'Ready', 'Monkey', 'Elevator', 'Wrench', 'Grease', 'Head', 'Theme', 'Grand', 'Cool', 'Kid', 'Boy', 'Girl', 'Vortex', 'Paradox'
+];
+
+export default class PlatformEventSample extends LightningElement {
+  nickname = nameList[Math.floor(Math.random() * nameList.length)];
+  message = `Welcome to Copipe Lab! Your Nickame is "${this.nickname}"! \nTry Messaging!`;
+  @track
+  logs = [];
+  timeoutId;
+
+  connectedCallback() {
+    subscribe(channel, -1, (event) => {
+      console.log(JSON.stringify(event.data.payload));
+      console.log("logs", JSON.stringify(this.logs));
+      this.logs.unshift(event);
+    });
+  }
+
+  handleInput(event) {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+    }
+    this.timeoutId = setTimeout(() => {
+      publish({ nickname: this.nickname, message: event.detail.value }).then(
+        (res) => console.log(res)
+      );
+    }, COMMAND_DELAY);
+  }
+}
